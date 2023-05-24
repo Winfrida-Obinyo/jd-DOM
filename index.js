1f4b162edabed7cf0a1c1e84ac3a869e3207f156
Winfida-Obinyo <obinyowinfridaa@gmail.com>
2023-05-24 16:53:42 +0300

    added spinach

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ document.getElementById("vegetables").style.textTransform = "uppercase"
 
 //  Add one more fruit to the fruits list
 let li = document.createElement('li');
-li.innerHTML= "dates";
+li.innerHTML= "Dates";
 document.getElementById('fruList').appendChild(li); 
 
 // Add one more vegetable to the vegetables
 // list
+// let li2 = document.createElement('li');
+// li2.innerHTML = "Spinach"
+// vegList.getElementById('vegList').appendChild('li2')
 
+let greens = document.createElement('li')
+greens.innerHTML = 'Spinach'
+vegList.appendChild(greens)
