4062bd43a26750404895f4ddfb0a56f83ae8b91e
Winfida-Obinyo <obinyowinfridaa@gmail.com>
2023-05-24 16:26:48 +0300

    doms commits

---
+++ index.js
@@ -0,0 +1,18 @@
+document.body.style.backgroundColor="silver";
+
+document.getElementById("title").style.color = "green"
+
+// Change the font case for h3 title tags to
+// uppercase
+document.getElementById("fruits").style.textTransform = "uppercase"
+document.getElementById("vegetables").style.textTransform = "uppercase"
+
+
+//  Add one more fruit to the fruits list
+let li = document.createElement('li');
+li.innerHTML= "dates";
+document.getElementById('fruList').appendChild(li); 
+
+// Add one more vegetable to the vegetables
+// list
+
